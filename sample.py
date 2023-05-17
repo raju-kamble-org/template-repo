34bc4f4f5cdb92baa42a539c3c27e3dadb2d9bab
raju-kamble <125890460+raju-kamble@users.noreply.github.com>
2023-05-17 20:43:10 +0530

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,10 @@
+import requests
+import commands
+template_vars['output'] = commands.getstatusoutput('/usr/bin/process_soemthing')
+
+# vulnerable code
+username = request.params.get('username');
+password = request.params.get('password');
+
+AWS_CREDENTIALS = { 'key': 'djekjdkejde', 'secret': 'dncndmncdmncd' }
+password = "12jkdgfkgl"
